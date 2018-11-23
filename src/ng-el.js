fa55f3a38d959cb355d1533796171bc72a9bb9c1
Alaude30 <kuramamode11@gmail.com>
2018-11-23 18:56:18 +0800

    Changes : All events now are working. but still it is in progress

--- src/ng-el.js
+++ src/ng-el.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(http);
 const rooms = [];
 const users = [];
 let roomId = 1;
+
 // will listen to 'connection' event,
 // meaning if there is someone connected to this server.
 io.on('connection', (socket) => {
@@ -19,24 +20,37 @@ io.on('connection', (socket) => {
 	};
 
 	socket.on('addUser', (user) => {
-		users.push(user);
+		// update users collection.
+		users.push(user.name);
+
+		// for login function.
+		socket.emit('user', user);
+
 		// this will inform everyone except the user itself.
+		console.log('User :: ' + user.name + ' has join.');
 		socket.broadcast.emit('users', users);
 	});
 
-	// enter room
+	/**
+	 * this will listen to 'enterRoom' event and emit 'room + id' event.
+	 */
 	socket.on('enterRoom', (id) => {
 		join(id);
 
 		// find the room.
-		const room = rooms.find((room) => room.id === id);
+		const room = rooms.find((room) => id === room.id);
 		console.log('Enter Room :: ', room);
+		console.log('Rooms :: ', rooms);
 
 		// emit room event so user can enter
-		socket.emit('room', room);
+		socket.emit('room' + id, room);
 	});
 
-	// createRoom
+	/**
+	 * this will listen to 'addRoom' event,
+	 * emit 'room + id' event and the client can listen to this.
+	 * this is design to create channel.
+	 */
 	socket.on('addRoom', (room) => {
 		// addRoom dont emit room with id.
 		join(roomId);
@@ -46,27 +60,39 @@ io.on('connection', (socket) => {
 		rooms.push(room);
 
 		// will emit room event so user that create room,
-		// will automaticall enter themselves.
-		socket.emit('room', room);
+		// will automatically enter themselves.
+		socket.emit('room' + roomId, room);
 
 		// this will send all the rooms to users connected to this server.
 		io.emit('rooms', rooms);
 
+		console.log('Created Room ::', room);
 		// prepare roomId for next create.
 		roomId++;
 	});
 
-	// newMessage
+	/**
+	 * this will listen to chat event and emit the updated data in to specific room.
+	 */
 	socket.on('chat', (room) => {
 		// find the room.
-		const currentRoom = rooms.find((room) => room.id === room.id);
+		const currentRoom = rooms.find((res) => res.id === room.id);
 
 		// update room for massages.
-		Object.assign(currentRoom, room);
-		console.log(currentRoom);
+		const result = Object.assign(currentRoom, room);
+
+		// this will be recieved by socket.
+		socket.emit('room' + room.id, result);
+
+		// this will be recieved by everyone who listen to this room in 'room + id' event.
+		socket.broadcast.emit('room' + room.id, result);
+
+		console.log(result);
 	});
 
-	// this will emit rooms to everyone connected.
+	// this will emit rooms to all socket,
+	// meanning this will be broadcast by the node server.
+	// note: this is not equall to socket.broadcast.
 	io.emit('rooms', rooms);
 });
 
